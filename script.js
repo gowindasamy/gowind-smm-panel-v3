887f9066b326402fc12aae3f9a821ea4bf2ce66d
Gowinda Samy <gowindasamycse@gmail.com>
2026-07-10 18:58:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ console.log("Gowind SMM Panel V3.0 Loaded");
 function showMessage() {
     alert("Welcome to Gowind SMM Panel!");
 }
+const form = document.getElementById("loginForm");
+
+if (form) {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username === "admin" && password === "admin123") {
+            window.location.href = "dashboard.html";
+        } else {
+            document.getElementById("message").innerHTML =
+                "❌ Invalid Username or Password";
+        }
+    });
+}
