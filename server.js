f03c36ef9b001e4b26e35ecca7acfac153c4101c
Gowinda Samy <gowindasamycse@gmail.com>
2026-07-10 19:17:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Gowind SMM Panel V3.0 Backend Running");
+  res.json({
+    status: "online",
+    name: "Gowind SMM Panel V3.0",
+    version: "3.0.1"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server Started on Port " + PORT);
+  console.log(`Server running on ${PORT}`);
 });
